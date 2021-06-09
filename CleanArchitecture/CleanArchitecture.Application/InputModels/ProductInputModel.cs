deb950b993ca121c6421bdf7e60e76a8d36db1ab
Michael Peter <michael_piterct@hotmail.com>
2021-06-08 21:01:11 -0300

    Product Model

---
+++ CleanArchitecture/CleanArchitecture.Application/InputModels/ProductInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Application.InputModels
+{
+    public class ProductInputModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
